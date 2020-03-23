0d89645cd9ee4eab02a0f4c69b205f0936dc1013
Xiaomeng Li <xmengli999@gmail.com>
2020-03-23 12:25:08 -0700

    update scripts

--- messidor_scripts/train_fold.sh
+++ messidor_scripts/train_fold.sh
@@ -5,5 +5,5 @@ do
     NUM="${var}$i"
     FILE="${var}/multi_CAN_lamda25_noshare_wpre_simpleaug_10fold$NUM""_1000"
     FOLD="${var}fold$NUM"
-    CUDA_VISIBLE_DEVICES='3' python baseline.py ./data/ missidor exp/MESSIDOR/$FILE --fold_name $FOLD -a resnet50 --gpu 0 -b 40 --base_lr 3e-4 --pretrained --epochs 1000  --decay_epoch 500 --num_class 2 --adam --multitask --crossCBAM --choice both --lambda_value 0.25  --resume KAGGLE/model_61/model_converge.pth.tar
+    CUDA_VISIBLE_DEVICES='3' python baseline.py ./data/ missidor exp/MESSIDOR/$FILE --fold_name $FOLD -a resnet50 --gpu 0 -b 40 --base_lr 3e-4 --pretrained --epochs 1000  --decay_epoch 500 --num_class 2 --adam --multitask --crossCBAM --choice both --lambda_value 0.25
 done
